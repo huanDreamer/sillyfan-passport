88cb6a6fd501547a018df38ef4d788d583e20ca4
huan <huan.dreamer@gmail.com>
2018-06-09 22:47:48 +0800

    功能完善

--- src/main/java/top/sillyfan/security/service/JwtUserDetailsService.java
+++ src/main/java/top/sillyfan/security/service/JwtUserDetailsService.java
@@ -39,6 +39,7 @@ public class JwtUserDetailsService implements UserDetailsService {
                 .superUser(user.getSuperUser())
                 .authorities(user.getAuthorizes())
                 .status(user.getStatus())
+                .online(user.getOnline())
                 .lastPasswordResetDate(user.getLastPasswordResetDate())
                 .build();
         }
